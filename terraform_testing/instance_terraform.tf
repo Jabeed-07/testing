d7c167e478153c9ef79a5e50ee7e163dd36be042
Jabeed-07 <jabeedgit@gmail.com>
2026-02-12 09:11:08 +0000

    added terraform aws instance

---
+++ terraform_testing/instance_terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-073130f74f5ffb161"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
+
